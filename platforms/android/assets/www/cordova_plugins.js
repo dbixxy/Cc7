bcbdd215557161081024f56116f891b94a5b669a
dbixxy <dbixxy@gmail.com>
2017-03-22 13:54:16 +0700

    initial commit

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "org.devgeeks.Canvas2ImagePlugin.Canvas2ImagePlugin",
+        "file": "plugins/org.devgeeks.Canvas2ImagePlugin/www/Canvas2ImagePlugin.js",
+        "pluginId": "org.devgeeks.Canvas2ImagePlugin",
+        "clobbers": [
+            "window.canvas2ImagePlugin"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.devgeeks.Canvas2ImagePlugin": "0.6.0"
+};
+// BOTTOM OF METADATA
+});
